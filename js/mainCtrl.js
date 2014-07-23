d5e164c96075ba0f5a0030149ce77d5d98ade775
Jake Overall <joverall22@gmail.com>
2014-07-22 20:57:34 -0600

    Chat Challenge Finished

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -11,12 +11,21 @@ app.controller('mainCtrl', function($scope, parseService){
   //The postData function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
   //pass that text to the postData method on the parseService object which will then post it to the parse backend.
 
+    $scope.getParseData = function() {
+        parseService.getData().then(function(results) {
+            $scope.messages = results;
+        });
+    };
 
+    $scope.postData = function () {
+        parseService.postData($scope.message);
+    };
+    
 
 
   //uncomment this code when your getParseData function is finished
   //This goes and gets new data every second, which mimicking a chat room experience.
-  // setInterval(function(){
-  //   $scope.getParseData();
-  // }, 1000)
+    setInterval(function() {
+        $scope.getParseData();
+    }, 1000);
 })
